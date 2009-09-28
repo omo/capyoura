f7d6c66c201f4543a22d35201d403ac251f4ed93
MORITA Hajime <omo@dodgson.org>
2009-09-29 01:50:40 +0900

    shutdown capped tab after timeout

--- crx/capyoura/content.js
+++ crx/capyoura/content.js
@@ -2,7 +2,9 @@
 /* */
 
 var VISIT_DELAY = 500; // effort to skip redirection
-var port = chrome.extension.connect();
+var SHUTDOWN_COUNT = 5;
+
+var g_port = chrome.extension.connect();
 
 function ensureElement(tagName, id) {
     var e = document.getElementById(id);
@@ -30,6 +32,26 @@ function showMeterTooltip(cap)
     document.body.appendChild(el);
 }
 
+function requestClose()
+{
+    g_port.postMessage({ type: "close" });
+}
+
+var g_count = SHUTDOWN_COUNT;
+
+function countdownCapScreen()
+{
+    g_count--;
+    if (g_count < 0) {
+	requestClose();
+	return;
+    }
+
+    var counterEl = document.getElementById("cyaCounter");
+    counterEl.innerHTML = g_count;
+    window.setTimeout(countdownCapScreen, 1000);
+}
+
 function showCapScreen(cap)
 {
     var el = ensureElement("div", "capScreen");
@@ -37,13 +59,16 @@ function showCapScreen(cap)
 		    "position:fixed; z-index: 1000; left: 2%; top:2%; min-width:84%; height:80%;" +
 		    "background-color:white; padding: 0.5em; padding-top: 10%; padding-left: 10%;" +
 		    "border-style: dashed; border-width:5px; border-color: black; font-family: verdana;" +
-		    "opacity: 0.9;");
+		    "");
     
     var linkPanelStyle = ("padding-left:1e; padding-top: 0.5em; padding-bottom:0.5em; font-size: 14pt;" +
 			  "text-align:left;");
     var mainLinkStyle = "color: #2276bb; font-weight: bold; font-size: 20pt;";
     var linkStyle = "color: #2276bb;";
-
+    var counterStyle = ("color: #2276bb; " +
+			"font-weight: bold; font-size: 38px; width:1.5em; text-align: center;" +
+			"position: fixed; top: 70%; left: 80%; " + 
+			"border-style: solid; border-color: #2276bb; border-width: 5pt; padding: 0.2em");
 
     el.innerHTML = ("<div style='text-align:left; font-family: verdana; color: #900;"  +
  		    "font-weight: bold; font-size: 32pt; padding-bottom:1em; '>" +
@@ -56,16 +81,18 @@ function showCapScreen(cap)
 		    "<div style='" + linkPanelStyle + "'>" +
 		    "<a id='cyaNothankyou' style='" + linkStyle + "' href='#'>&raquo; No thank you. I'm a webholic afterall...</a>" +
 		    "</div>" +
-
 		    "<div style='" + linkPanelStyle + "'>" +
 		    "<a id='cyaDashboard' style='" + linkStyle + "' href='http://capyoura.appspot.com/dashboard'>&raquo; Open dashboard.</a>" +
+		    "</div>" +
+		    "<div id='cyaCounter' style='" + counterStyle + "'>" +
+		    g_count +
 		    "</div>"
 		   );
     document.body.appendChild(el);
 
     document.getElementById("cyaTakeaway").addEventListener
     ("click", function(evt) {
-	port.postMessage({ type: "close" });
+	requestClose();
 	evt.preventDefault();
     });
 
@@ -74,12 +101,13 @@ function showCapScreen(cap)
 	document.getElementById("capScreen").setAttribute("style", "display: none;");
 	evt.preventDefault();
     });
-    
+
+    window.setTimeout(countdownCapScreen, 1000);
 }
 
 function initialize()
 {
-    port.onMessage.addListener(function(cap) {
+    g_port.onMessage.addListener(function(cap) {
 	console.log("cap:" + JSON.stringify(cap));
 	if (cap.visitCount < cap.limit) {
 	    showMeterTooltip(cap);
@@ -90,7 +118,7 @@ function initialize()
 
     window.setTimeout(function() {
 	if (window.parent == window) { // only visit root frame
-	    port.postMessage({ type: "opened", uri: document.location.href });
+	    g_port.postMessage({ type: "opened", uri: document.location.href });
 	}
     }, VISIT_DELAY);
 }
