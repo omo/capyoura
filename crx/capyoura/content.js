57b0ccb5d059f2355d20614ddea12807ecd48036
MORITA Hajime <omo@dodgson.org>
2009-09-29 00:43:05 +0900

    dogfooded fix - guard iframe - attempt to guard redirection - make screens position: fixed. - other few tweeks

--- crx/capyoura/content.js
+++ crx/capyoura/content.js
@@ -1,17 +1,29 @@
 
 /* */
 
+var VISIT_DELAY = 500; // effort to skip redirection
 var port = chrome.extension.connect();
+
+function ensureElement(tagName, id) {
+    var e = document.getElementById(id);
+    if (e) {
+	e.parent.removeChild(e);
+	return e;
+    }
+
+    e = document.createElement(tagName);
+    e.setAttribute("id", id);
+    return e;
+}
 
 function showMeterTooltip(cap)
 {
-    var el = document.createElement("div");
-    el.setAttribute("id", "addictionMeter");
+    var el = ensureElement("div", "addictionMeter");
     var level = (cap.visitCount + 1) / (cap.limit + 1);
     el.setAttribute("style", 
-		    "position:absolute; right: 1em; " +
+		    "position:fixed; right: 1em; z-index: 1000; " +
 		    "top: 5%; background-color:red; color: white; padding: 0.5em;" +
-		    "line-height: 1.5em;" +
+		    "font-family: verdana; font-weight: bold; line-height: 1.5em;" +
 		    "opacity: " + level + ";");
     el.innerHTML = ("<div>addicting...</div>" +
 		    "<div>" + cap.visitCount + "/" + cap.limit + "</div>");
@@ -20,13 +32,11 @@ function showMeterTooltip(cap)
 
 function showCapScreen(cap)
 {
-    // XXX: extract styles to seaprate file.
-    var el = document.createElement("div");
-    el.setAttribute("id", "capScreen");
+    var el = ensureElement("div", "capScreen");
     el.setAttribute("style", 
-		    "position:absolute; left: 2%; top:2%; min-width:84%; height:80%;" +
+		    "position:fixed; z-index: 1000; left: 2%; top:2%; min-width:84%; height:80%;" +
 		    "background-color:white; padding: 0.5em; padding-top: 10%; padding-left: 10%;" +
-		    "border-style: dashed; border-width:5px; border-color: black;" +
+		    "border-style: dashed; border-width:5px; border-color: black; font-family: verdana;" +
 		    "opacity: 0.9;");
     
     var linkPanelStyle = ("padding-left:1e; padding-top: 0.5em; padding-bottom:0.5em; font-size: 14pt;" +
@@ -67,13 +77,23 @@ function showCapScreen(cap)
     
 }
 
-port.onMessage.addListener(function(cap) {
-    console.log("cap:" + JSON.stringify(cap));
-    if (cap.visitCount < cap.limit) {
-	showMeterTooltip(cap);
-    } else {
-	showCapScreen(cap);
-    }
-});
+function initialize()
+{
+    port.onMessage.addListener(function(cap) {
+	console.log("cap:" + JSON.stringify(cap));
+	if (cap.visitCount < cap.limit) {
+	    showMeterTooltip(cap);
+	} else {
+	    showCapScreen(cap);
+	}
+    });
 
-port.postMessage({ type: "opened", uri: document.location.href });
+    window.setTimeout(function() {
+	if (window.parent == window) { // only visit root frame
+	    port.postMessage({ type: "opened", uri: document.location.href });
+	}
+    }, VISIT_DELAY);
+}
+
+initialize();
+
